34d73f427d59c54bd9b00b36a7beccaa85e346fd
Baptiste Langlade <baptouuuu@gmail.com>
2016-07-22 08:59:12 +0200

    add command to list budgets

---
+++ src/Command/Budget/ListCommand.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types = 1);
+
+namespace ExpenseManager\Cli\Command\Budget;
+
+use ExpenseManager\{
+    Repository\BudgetRepositoryInterface,
+    Repository\CategoryRepositoryInterface,
+    Entity\Budget,
+    Entity\Category\IdentityInterface
+};
+use Innmind\Immutable\Set;
+use Symfony\Component\Console\{
+    Command\Command,
+    Input\InputInterface,
+    Output\OutputInterface
+};
+
+final class ListCommand extends Command
+{
+    private $budgetRepository;
+    private $categoryRepository;
+
+    public function __construct(
+        BudgetRepositoryInterface $budgetRepository,
+        CategoryRepositoryInterface $categoryRepository
+    ) {
+        $this->budgetRepository = $budgetRepository;
+        $this->categoryRepository = $categoryRepository;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('budget:list');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this
+            ->budgetRepository
+            ->all()
+            ->reduce(
+                $output,
+                function(OutputInterface $output, Budget $budget): OutputInterface {
+                    $output->writeln(sprintf(
+                        '%s (%s for %s)',
+                        $budget->name(),
+                        round($budget->amount()->value() / 100, 2),
+                        $budget
+                            ->categories()
+                            ->reduce(
+                                new Set('string'),
+                                function(Set $carry, IdentityInterface $identity): Set {
+                                    return $carry->add(
+                                        $this
+                                            ->categoryRepository
+                                            ->get($identity)
+                                            ->name()
+                                    );
+                                }
+                            )
+                            ->join(', ')
+                    ));
+
+                    return $output;
+                }
+            );
+    }
+}
