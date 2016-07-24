ba1b88a5ccdafe5aebe29f4f6b66b11f29a5b549
Baptiste Langlade <baptouuuu@gmail.com>
2016-07-24 11:10:20 +0200

    add command to list one-off incomes

--- src/Command/OneOffIncome/ListCommand.php
+++ src/Command/OneOffIncome/ListCommand.php
@@ -1,4 +1,75 @@
-//same structure as expense:list
-//then do store domain events
-//do command bus bundle
-//do event bus bundle
+<?php
+declare(strict_types = 1);
+
+namespace ExpenseManager\Cli\Command\OneOffIncome;
+
+use ExpenseManager\{
+    Repository\OneOffIncomeRepositoryInterface,
+    Entity\OneOffIncome,
+    Specification\OneOffIncome\After,
+    Specification\OneOffIncome\Before
+};
+use Symfony\Component\Console\{
+    Command\Command,
+    Input\InputOption,
+    Input\InputInterface,
+    Output\OutputInterface
+};
+
+final class ListCommand extends Command
+{
+    private $repository;
+    private $categoryRepository;
+
+    public function __construct(OneOffIncomeRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('one-off-income:list')
+            ->addOption(
+                'from',
+                'f',
+                InputOption::VALUE_REQUIRED,
+                'Since when to list',
+                '-1 month'
+            )
+            ->addOption(
+                'to',
+                't',
+                InputOption::VALUE_REQUIRED,
+                'When to list up to',
+                'today'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this
+            ->repository
+            ->matching(
+                (new After(
+                    new \DateTime($input->getOption('from'))
+                ))->and(new Before(
+                    new \DateTime($input->getOption('to'))
+                ))
+            )
+            ->reduce(
+                $output,
+                function(OutputInterface $output, OneOffIncome $income): OutputInterface {
+                    $output->writeln(sprintf(
+                        '[<fg=yellow>%s</>] %01.2f (note: %s)',
+                        $income->date()->format('Y-m-d'),
+                        $income->amount()->value() / 100,
+                        $income->note()
+                    ));
+
+                    return $output;
+                }
+            );
+    }
+}
