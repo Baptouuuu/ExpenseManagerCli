a74cd0e34f71e3b73e775891e8010d7f0162edfa
Baptiste Langlade <baptouuuu@gmail.com>
2016-07-29 07:50:49 +0200

    add listener to apply fixed costs

---
+++ src/EventListener/MonthReport/ApplyFixedCostListener.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types = 1);
+
+namespace ExpenseManager\Cli\EventListener\MonthReport;
+
+use ExpenseManager\{
+    Repository\FixedCostRepositoryInterface,
+    Entity\FixedCost,
+    Command\MonthReport\ApplyFixedCost,
+    Cli\Entity\MonthReport\Identity
+};
+use Innmind\CommandBus\CommandBusInterface;
+use Symfony\Component\{
+    EventDispatcher\EventSubscriberInterface,
+    Console\ConsoleEvents,
+    Console\Event\ConsoleCommandEvent
+};
+
+final class ApplyFixedCostListener implements EventSubscriberInterface
+{
+    private $bus;
+    private $repository;
+
+    public function __construct(
+        CommandBusInterface $bus,
+        FixedCostRepositoryInterface $repository
+    ) {
+        $this->bus = $bus;
+        $this->repository = $repository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            ConsoleEvents::COMMAND => [['applyFixedCosts', 50]],
+        ];
+    }
+
+    public function __invoke(FixedCostWasCreated $event)
+    {
+        if ($event->applyDay()->value() !== (int) (new \DateTime)->format('j')) {
+            return;
+        }
+
+        $this->bus->handle(
+            new ApplyFixedCost(
+                new Identity((new \DateTime)->format('Y-m')),
+                $event->identity()
+            )
+        );
+    }
+
+    public function applyFixedCosts(ConsoleCommandEvent $event)
+    {
+        if (!$event->commandShouldRun()) {
+            return;
+        }
+
+        $this
+            ->repository
+            ->all()
+            ->filter(function(FixedCost $cost): bool {
+                return $cost->applyDay()->value() === (int) (new \DateTime)->format('j');
+            })
+            ->foreach(function(FixedCost $cost) {
+                $this->bus->handle(
+                    new ApplyFixedCost(
+                        new Identity((new \DateTime)->format('Y-m')),
+                        $cost->identity()
+                    )
+                );
+            });
+    }
+}
