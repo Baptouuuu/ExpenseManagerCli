cc2e485d7d3b014d4aa296dd3afa2af1a1a1494a
Baptiste Langlade <baptouuuu@gmail.com>
2016-07-29 08:45:35 +0200

    add listener to init the month report projection

---
+++ src/EventListener/Projector/MonthReport/InitProjectionListener.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types = 1);
+
+namespace ExpenseManager\Cli\EventListener\Projector\MonthReport;
+
+use ExpenseManager\Event\MonthReportWasCreated;
+use Innmind\Filesystem\{
+    AdapterInterface,
+    File,
+    Stream\StringStream
+};
+
+final class InitProjectionListener
+{
+    private $filesystem;
+
+    public function __construct(AdapterInterface $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+    public function __invoke(MonthReportWasCreated $event)
+    {
+        $this->filesystem->add(
+            new File(
+                (string) $event->date()->format('Y-m'),
+                new StringStream(
+                    json_encode([
+                        'raw_amount' => 0,
+                        'formatted_amount' => '<fg=green>0.00</>',
+                        'categories' => [],
+                        'raw_total_income' => 0,
+                        'formatted_total_income' => '<fg=green>0.00</>',
+                        'raw_total_expense' => 0,
+                        'formatted_total_expense' => '<fg=green>0.00</>',
+                    ])."\n"
+                )
+            )
+        );
+    }
+}
