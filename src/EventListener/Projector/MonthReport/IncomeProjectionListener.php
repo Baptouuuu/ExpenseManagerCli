07cd47616dabbbb71e0e7913a3eefc67e1f80d4b
Baptiste Langlade <baptouuuu@gmail.com>
2016-07-31 13:35:30 +0200

    add income pojection

---
+++ src/EventListener/Projector/MonthReport/IncomeProjectionListener.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+
+namespace ExpenseManager\Cli\EventListener\Projector\MonthReport;
+
+use ExpenseManager\{
+    Event\MonthReport\IncomeHasBeenApplied,
+    Repository\IncomeRepositoryInterface
+};
+use Innmind\Filesystem\AdapterInterface;
+
+final class IncomeProjectionListener
+{
+    use AmountUpdater;
+
+    private $filesystem;
+    private repository;
+
+    public function __construct(
+        AdapterInterface $filesystem,
+        IncomeRepositoryInterface $repository
+    ) {
+        $this->filesystem = $filesystem;
+        $this->repository = $repository;
+    }
+
+    public function __invoke(IncomeHasBeenApplied $event)
+    {
+        $file = $this->filesystem->get(
+            $event->date()->format('Y-m')
+        );
+        $income = $this->repository->get($event->income());
+        $file = $this->increaseBy($file, $income->amount()->value());
+        $this->filesystem->add($file);
+    }
+}
